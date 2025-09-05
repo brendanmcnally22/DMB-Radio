b0a8a687258530cef99d7a02849ea1b23a8378f9
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-09-05 13:18:37 -0400

    Add Script/Comment
    Add Gittest Script
    Add Code Comments

---
+++ Assets/GitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //Lets see if we can all add in some comments to test out our branching and merging skillzzz Woop woop
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
