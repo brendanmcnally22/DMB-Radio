68381b3e1d49ce42f478802525351a220a0cb210
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-09-16 08:56:41 -0400

    Clean up - no errors

--- Assets/Scripts/MicLoudness.cs
+++ Assets/Scripts/MicLoudness.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // We need this for our Slider! woo
+
 [RequireComponent(typeof(AudioSource))] // I need to make sure an Audio Source is always attached. This does that. :D Thank you! 
 public class MicLoudness : MonoBehaviour
 {
@@ -22,10 +22,6 @@ public class MicLoudness : MonoBehaviour
     public float rms; // root mean square, this will be like how overall loud it will be
     public float peak; // max amp in this current analysis block
     public float db; // Decibels, im using logarithmic scale, We converted to decibels, Thank you decibels....
-
-    // UI slider!!! (important for immersion) 
-    public Slider meter; 
-    public float 
 
     void Start()
     {
